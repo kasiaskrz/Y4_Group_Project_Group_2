743dce33d65e2ce4030f7b44296774f2a192fbc6
SAULESMALL <114486250+SAULESMALL@users.noreply.github.com>
2025-11-04 10:06:37 +0000

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm')?.addEventListener('submit', e => {
+  e.preventDefault();
+  alert('Logging in...');
+});
+
+document.getElementById('registerForm')?.addEventListener('submit', e => {
+  e.preventDefault();
+  alert('Account created!');
+});
