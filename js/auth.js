a9e8b92f71637c2ca92535a5be27d96734cbe3a1
Kasia <k_skrzynska@onet.pl>
2025-11-21 19:30:19 +0000

    auth

--- js/auth.js
+++ js/auth.js
@@ -3,6 +3,7 @@ const supabase = window.supabase.createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3dXlmZ3Z0YXpxeXN4bm12bmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MzQyMzAsImV4cCI6MjA3ODExMDIzMH0.qOpekOPmKweh29QgtQUCGM-fAXPJZ58R0ccSjMET-rM"
 );
 
+const registerForm = document.getElementById("registerForm");
 // --- REGISTER ---
 async function registerUser(email, password) {
     const { data, error } = await supabase.auth.signUp({
