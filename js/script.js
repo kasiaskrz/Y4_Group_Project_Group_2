b33609188f39759261521eb018f6a34174c9361a
Kasia <k_skrzynska@onet.pl>
2026-01-15 16:16:24 +0000

    updated supabase link

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const supabase = window.supabase.createClient(
   "https://ywuyfgvtazqysxnmvnkv.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3dXlmZ3Z0YXpxeXN4bm12bmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MzQyMzAsImV4cCI6MjA3ODExMDIzMH0.qOpekOPmKweh29QgtQUCGM-fAXPJZ58R0ccSjMET-rM"
 );
+window.supabaseClient = supabase;
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   const { data: { user } } = await supabase.auth.getUser();
@@ -9,7 +11,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const loginBtn = document.getElementById("btn-login");
   const registerBtn = document.getElementById("btn-register");
   const logoutBtn = document.getElementById("btn-logout");
-  const profileBtn = document.getElementById("btn-profile"); // NEW
+  const profileBtn = document.getElementById("btn-profile"); 
 
   if (user) {
     // Logged in
