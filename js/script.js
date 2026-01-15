6394de970696b89e72cfed25b15cfc0d745f3f08
Kasia <k_skrzynska@onet.pl>
2026-01-15 17:16:50 +0000

    Fix Login

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   "https://ywuyfgvtazqysxnmvnkv.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3dXlmZ3Z0YXpxeXN4bm12bmt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MzQyMzAsImV4cCI6MjA3ODExMDIzMH0.qOpekOPmKweh29QgtQUCGM-fAXPJZ58R0ccSjMET-rM"
 );
-window.supabaseClient = supabase;
 
 
 document.addEventListener("DOMContentLoaded", async () => {
+  const supabase = window.supabaseClient; 
+
   const { data: { user } } = await supabase.auth.getUser();
 
   const loginBtn = document.getElementById("btn-login");
